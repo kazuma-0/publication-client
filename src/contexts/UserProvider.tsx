58bfad0361faf14a6e13069f96393367e147b16d
kazuma-0 <hello@kazuma.in>
2023-02-05 13:00:17 +0530

    update provider

--- src/contexts/UserProvider.tsx
+++ src/contexts/UserProvider.tsx
@@ -1,36 +1,34 @@
-import {User} from "@/lib";
+import { User } from "@/lib";
 import jwtDecode from "jwt-decode";
-import {useRouter} from "next/router";
-import React, {useContext, useEffect, useState} from "react";
+import { useRouter } from "next/router";
+import React, { useContext, useEffect, useState } from "react";
 
 // @ts-ignore
 const UserContext = React.createContext<User>();
 
 export const useUser = () => {
-    return useContext(UserContext);
+	return useContext(UserContext);
 };
 
 export default function UserProvider({
-                                         children,
-                                     }: {
-    children: React.ReactNode;
+	children,
+}: {
+	children: React.ReactNode;
 }) {
-    const router = useRouter();
-    // const [user, setUser] = useState<User>();
-    const token = localStorage.getItem('token')
-    useEffect(()=>{
-        if (!token && router.pathname.includes('dashboard')) {
-                router.push("/");
-                return;
-        } else {
-            // @ts-ignore
-            // setUser(jwtDecode(localStorage.getItem("token")))
-        }
-    }, [router.pathname])
-    const [user, setUser] = useState<User>(
-    	jwtDecode(localStorage.getItem("token") ?? "")
-    );
+	const router = useRouter();
+	const [user, setUser] = useState<User>();
+	const token = localStorage.getItem("token");
+	useEffect(() => {
+		if (!token && router.pathname.includes("dashboard")) {
+			router.push("/");
+			return;
+		}
+		if (token) {
+			// @ts-ignore
+			setUser(jwtDecode(token));
+		}
+	}, [router.pathname]);
 
-    // @ts-ignore
-    return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+	// @ts-ignore
+	return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
 }
